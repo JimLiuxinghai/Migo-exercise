20f773defe3f78dfad72b187bcf64b7478a8dea4
jim liu <jimliuxinghai@163.com>
2016-01-14 22:14:31 +0800

    second
    usercenter complate

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,16 @@ router.get('/', function(req, res, next) {
 });
 /*登录注册*/
 router.get('/login',function(req,res,next){
-  res.render('login',{title:"Migo个人健身系统"});
-})
+  res.render('login',{title:"Migo个人健身系统--登录"});
+});
 router.get('/reg',function(req,res,next){
-  res.render('reg',{title:"Migo个人健身系统"});
-})
+  res.render('reg',{title:"Migo个人健身系统--注册"});
+});
+/*个人中心*/
+router.get('/usercenter',function(req,res,next){
+  res.render('usercenter',{title:"Migo个人健身系统--个人中心"});
+});
+router.get('/setprofile',function(req,res,next){
+    res.render('setprofile',{title:"Migo个人健身系统--个人中心"});
+});
 module.exports = router;
