4447badee896d4de8940685eb3c14387d85f6e58
jim liu <jimliuxinghai@163.com>
2016-03-23 23:35:36 +0800

    plane

---
+++ public/javascripts/model/adminPlane.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jimliu on 2016/3/19.
+ */
+require(['jquery','highcharts','util','domReady'],function($,highcharts,util,domReady){
+    domReady(function(){
+        //页面初始化
+        var ready = function(){
+            //获取屏幕高度
+            util.getScreen($(".left"),80);
+        }();
+
+    });
+
+})
